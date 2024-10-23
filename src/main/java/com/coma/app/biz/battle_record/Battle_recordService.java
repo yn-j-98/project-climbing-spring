4faeabb54f199d1e2548248b25220562f8984c0d
developerSeonah <salj3895.developer@gmail.com>
2024-10-23 18:47:29 +0900

    다시 합치기

--- src/main/java/com/coma/app/biz/battle_record/Battle_recordService.java
+++ src/main/java/com/coma/app/biz/battle_record/Battle_recordService.java
@@ -1,12 +1,16 @@
 package com.coma.app.biz.battle_record;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public interface Battle_recordService {
-	
-	ArrayList<Battle_recordDTO> selectAll(Battle_recordDTO battle_recordDTO);
-	Battle_recordDTO selectOne(Battle_recordDTO battle_recordDTO);
-	boolean update(Battle_recordDTO battle_recordDTO);
+	List<Battle_recordDTO> selectAllWinner(Battle_recordDTO battle_recordDTO);
+	List<Battle_recordDTO> selectAllParticipantCrew(Battle_recordDTO battle_recordDTO);
+	List<Battle_recordDTO> selectAllParticipantBattle(Battle_recordDTO battle_recordDTO);
+	List<Battle_recordDTO> selectAllWinnerParticipantGym(Battle_recordDTO battle_recordDTO);
+	Battle_recordDTO selectOneBattle(Battle_recordDTO battle_recordDTO);
+	Battle_recordDTO selectOneBattleRecord(Battle_recordDTO battle_recordDTO);
+	Battle_recordDTO selectOneCountCrew(Battle_recordDTO battle_recordDTO);
 	boolean insert(Battle_recordDTO battle_recordDTO);
+	boolean update(Battle_recordDTO battle_recordDTO);
 	boolean delete(Battle_recordDTO battle_recordDTO);
 }
--- src/main/java/com/coma/app/biz/crew_board/Crew_boardDAO.java
+++ src/main/java/com/coma/app/biz/crew_board/Crew_boardDAO.java
@@ -4,14 +4,17 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+import com.coma.app.biz.board.BoardDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+
+
 @Repository
 public class Crew_boardDAO {
-	//(페이지 네이션) 특정 크루 글 출력 CREW_BOARD_WRITER_ID, Crew_board_min_num, Crew_board_max_num
+	//(페이지 네이션) 특정 크루 글 출력 model_crew_board_min_num, model_crew_board_max_num, CREW_BOARD_WRITER_ID
 	private final String ALL_CREW_BOARD = "SELECT CB.CREW_BOARD_NUM, CB.CREW_BOARD_WRITER_ID, CB.CREW_BOARD_TITLE, " +
 			"CB.CREW_BOARD_CONTENT, CB.CREW_BOARD_CNT, M.MEMBER_PROFILE " +
 			"FROM CREW_BOARD CB " +
@@ -49,135 +52,150 @@ public class Crew_boardDAO {
 	private final String ONE = "SELECT CREW_BOARD_NUM, CREW_BOARD_WRITER_ID, CREW_BOARD_CONTENT, CREW_BOARD_CNT FROM CREW_BOARD WHERE CREW_BOARD_NUM = ?";
 
 	@Autowired
-	private JdbcTemplate jdbcTemplate; // 스프링부트 내장객체
-	
+	private JdbcTemplate jdbcTemplate;
+
 	public boolean insert(Crew_boardDTO crew_boardDTO) {//기능 미구현
 		System.out.println("com.coma.app.biz.crew_board.insert 시작");
-		//크루 게시판 글 작성 CREW_BOARD_WRITER_ID, CREW_BOARD_CONTENT, CREW_BOARD_TITLE
-		int result=jdbcTemplate.update(INSERT,crew_boardDTO.getCrew_board_writer_id(),crew_boardDTO.getCrew_board_content(),crew_boardDTO.getCrew_board_title());
-		if(result<=0) {
-			System.out.println("com.coma.app.biz.crew_board.insert SQL문 실패");
+		int result = jdbcTemplate.update(INSERT, crew_boardDTO.getCrew_board_writer_id(), crew_boardDTO.getCrew_board_content(), crew_boardDTO.getCrew_board_title());
+		if (result <= 0) {
+			System.err.println("	[에러]com.coma.app.biz.crew_board.insert sql 실패 : insert = " + INSERT);
 			return false;
 		}
-		System.out.println("com.coma.app.biz.crew_board.insert 성공");
 		return true;
 	}
-
-	private boolean update(Crew_boardDTO crew_boardDTO) { // TODO 여기없는 CRUD
+	public boolean update(Crew_boardDTO crew_boardDTO) {//기능 미구현
 		System.out.println("com.coma.app.biz.crew_board.update 시작");
 		System.out.println("com.coma.app.biz.crew_board.update 성공");
 		return true;
 	}
-
 	public boolean delete(Crew_boardDTO crew_boardDTO) {
 		System.out.println("com.coma.app.biz.crew_board.delete 시작");
-		//크루 게시판 글 삭제 CREW_NUM
-		int result=jdbcTemplate.update(DELETE,crew_boardDTO.getCrew_board_num());
-		if(result<=0) {
-			System.out.println("com.coma.app.biz.crew_board.delete SQL문 실패");
+		int result = jdbcTemplate.update(DELETE, crew_boardDTO.getCrew_board_num());
+		if (result <= 0) {
+			System.err.println("	[에러]com.coma.app.biz.crew_board.delete sql 실패 : UPDATE = " + DELETE);
 			return false;
 		}
-		System.out.println("com.coma.app.biz.crew_board.delete 성공");
 		return true;
 	}
 
+	//PK로 검색 CREW_BOARD_NUM
 	public Crew_boardDTO selectOne(Crew_boardDTO crew_boardDTO) {
-		System.out.println("com.coma.app.biz.crew_board.selectOne 시작");
-
-		Crew_boardDTO data = null;
-		Object[] args={crew_boardDTO.getCrew_board_num()};
+		System.out.println("	[로그]com.coma.app.biz.board.BoardDAO.selectOneSearchIdCount 시작");
+		Crew_boardDTO result = null;
+		Object[] args = {crew_boardDTO.getCrew_board_num()};
 		try {
-			//PK로 검색 CREW_BOARD_NUM
-			data=jdbcTemplate.queryForObject(ONE,args,new CrewBoardRowMapperOne());
+			result = jdbcTemplate.queryForObject(ONE, args, new crewBoardRowMapperOne());
+		} catch (Exception e) {
+			System.err.println("	[에러]com.coma.app.biz.board.BoardDAO.selectOneSearchIdCount 실패 " + e.getMessage());
+			e.printStackTrace();
 		}
-		catch(Exception e) {
-			System.out.println("com.coma.app.biz.crew_board.selectOne SQL문 실패");
-		}
-		System.out.println("com.coma.app.biz.crew_board.selectOne 성공");
-		return data;
+		return result;
 	}
 
-	public Crew_boardDTO selectOneCount(Crew_boardDTO crew_boardDTO) {
-		System.out.println("com.coma.app.biz.crew_board.selectOne 시작");
-
-		Crew_boardDTO data = null;
-		Object[] args={crew_boardDTO.getCrew_board_writer_id()};
+	//해당 크루 커뮤니티 전체글 총 개수 CREW_BOARD_WRITER_ID
+	public Crew_boardDTO selectOneCount(Crew_boardDTO Crew_boardDTO) {
+		System.out.println("com.coma.app.biz.crew_board.selectOneCount 시작");
+		Crew_boardDTO result = null;
+		Object[] args = {Crew_boardDTO.getCrew_board_num()};
 		try {
-			//해당 크루 커뮤니티 전체글 총 개수 CREW_BOARD_WRITER_ID
-			data=jdbcTemplate.queryForObject(ONE_COUNT,args,new CrewBoardCountRowMapperOne());
+			result = jdbcTemplate.queryForObject(ONE_COUNT, args, new crewBoardRowMapperOneCount());
+		} catch (Exception e) {
+			System.err.println("	[에러]com.coma.app.biz.crew_board.selectOneCount 실패 " + e.getMessage());
+			e.printStackTrace();
 		}
-		catch(Exception e) {
-			System.out.println("com.coma.app.biz.crew_board.selectOne SQL문 실패");
-		}
-		System.out.println("com.coma.app.biz.crew_board.selectOne 성공");
-		return data;
+		return result;
 	}
 
-	public List<Crew_boardDTO> selectAll(Crew_boardDTO crew_boardDTO) {
-		System.out.println("com.coma.app.biz.crew_board.selectAll 시작");
-
-		List<Crew_boardDTO> datas = null;
-		Object[] args={crew_boardDTO.getCrew_board_min_num(),10,crew_boardDTO.getCrew_board_writer_id()};
+	//(페이지 네이션) 특정 크루 글 출력 Crew_board_min_num, Crew_board_max_num, Crew_board_writer_id
+	public List<Crew_boardDTO> selectAllCrewBoard(Crew_boardDTO Crew_boardDTO) {
+		System.out.println("com.coma.app.biz.crew_board.selectAllCrewBoard 시작");
+		List<Crew_boardDTO> result = null;
+		int minNum = Crew_boardDTO.getCrew_board_min_num();
+		int offset = minNum; //페이지네이션 시작위치
+		Object[] args = {offset, 10, Crew_boardDTO.getCrew_board_writer_id()};
 		try {
-			//(페이지 네이션) 특정 크루 글 출력 CREW_BOARD_WRITER_ID, Crew_board_min_num, Crew_board_max_num
-			datas=jdbcTemplate.query(ALL_CREW_BOARD, args, new CrewBoardRowMapperAll());
+			result = jdbcTemplate.query(ALL_CREW_BOARD, args, new crewBoardRowAllCrewBoard());
+		} catch (Exception e) {
+			System.err.println("	[에러]com.coma.app.biz.crew_board.selectAllCrewBoard 실패 " + e.getMessage());
+			e.printStackTrace();
 		}
-		catch (Exception e) {
-			System.out.println("com.coma.app.biz.crew_board.selectAll SQL문 실패");
+		return result;
+	}
+
+
+}
+
+class crewBoardRowMapperOne implements RowMapper<Crew_boardDTO> {
+	@Override
+	public Crew_boardDTO mapRow(ResultSet resultSet, int i) throws SQLException {
+		System.out.println("com.coma.app.biz.crew_board.selectOne 검색 성공");
+		Crew_boardDTO crew_boardDTO = new Crew_boardDTO();
+		try {
+			crew_boardDTO.setCrew_board_num(resultSet.getInt("CREW_BOARD_NUM"));
+		} catch (Exception e) {
+			System.err.println("Board_num = 0");
+			crew_boardDTO.setCrew_board_num(0);
 		}
-		System.out.println("com.coma.app.biz.crew_board.selectAll 성공");
-		return datas;
+		try {
+			crew_boardDTO.setCrew_board_writer_id(resultSet.getString("CREW_BOARD_WRITER_ID"));
+		} catch (Exception e) {
+			System.err.println("Crew_board_writer_id = null");
+			crew_boardDTO.setCrew_board_writer_id(null);
+		}
+		try {
+			crew_boardDTO.setCrew_board_content(resultSet.getString("CREW_BOARD_CONTENT"));
+		} catch (Exception e) {
+			System.err.println("Crew_board_content = null");
+			crew_boardDTO.setCrew_board_content(null);
+		}
+		try {
+			crew_boardDTO.setCrew_board_cnt(resultSet.getInt("CREW_BOARD_CNT"));
+		} catch (Exception e) {
+			System.err.println("Crew_board_cnt = 0");
+			crew_boardDTO.setCrew_board_cnt(0);
+		}
+		return crew_boardDTO;
 	}
 }
 
-class CrewBoardRowMapperOne implements RowMapper<Crew_boardDTO> {
-
-	public Crew_boardDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
-		Crew_boardDTO crew_boardDTO=new Crew_boardDTO();
-		System.out.print("GymCountRowMapper DB에서 가져온 데이터 {");
-		crew_boardDTO.setCrew_board_num(rs.getInt("CREW_BOARD_NUM"));
-		System.err.println("crew_board_num = ["+crew_boardDTO.getCrew_board_num()+"]");
-		crew_boardDTO.setCrew_board_writer_id(rs.getString("CREW_BOARD_WRITER_ID"));
-		System.err.println("crew_board_writer_id = ["+crew_boardDTO.getCrew_board_writer_id()+"]");
-		crew_boardDTO.setCrew_board_content(rs.getString("CREW_BOARD_CONTENT"));
-		System.err.println("crew_board_content = ["+crew_boardDTO.getCrew_board_content()+"]");
-		crew_boardDTO.setCrew_board_cnt(rs.getInt("CREW_BOARD_CNT"));
-		System.err.print("crew_board_cnt = ["+crew_boardDTO.getCrew_board_cnt()+"]");
-		System.out.println("}");
+class crewBoardRowMapperOneCount implements RowMapper<Crew_boardDTO> {
+	@Override
+	public Crew_boardDTO mapRow(ResultSet resultSet, int i) throws SQLException {
+		System.out.println("com.coma.app.biz.crew_board.selectOneCount 검색 성공");
+		Crew_boardDTO crew_boardDTO = new Crew_boardDTO();
+		try{
+			crew_boardDTO.setCrew_board_total(resultSet.getInt("CREW_BOARD_TOTAL"));
+		}catch (Exception e){
+			System.err.println("Crew_board_total = null");
+			crew_boardDTO.setCrew_board_total(0);
+		}
 		return crew_boardDTO;
-	};
+	}
 }
 
-class CrewBoardCountRowMapperOne implements RowMapper<Crew_boardDTO> {
-
-	public Crew_boardDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
-		Crew_boardDTO crew_boardDTO=new Crew_boardDTO();
-		System.out.print("GymCountRowMapper DB에서 가져온 데이터 {");
-		crew_boardDTO.setCrew_board_total(rs.getInt("CREW_BOARD_TOTAL"));
-		System.err.print("crew_board_total = ["+crew_boardDTO.getCrew_board_total()+"]");
-		System.out.println("}");
+class crewBoardRowAllCrewBoard implements RowMapper<Crew_boardDTO> {
+	@Override
+	public Crew_boardDTO mapRow(ResultSet resultSet, int i) throws SQLException {
+		System.out.println("com.coma.app.biz.crew_board.selectAllCrewBoard 검색 성공");
+		Crew_boardDTO crew_boardDTO = new Crew_boardDTO();
+		try {
+			crew_boardDTO.setCrew_board_num(resultSet.getInt("CREW_BOARD_NUM"));
+		} catch (Exception e) {
+			System.err.println("Board_num = 0");
+			crew_boardDTO.setCrew_board_num(0);
+		}
+		try {
+			crew_boardDTO.setCrew_board_writer_id(resultSet.getString("CREW_BOARD_WRITER_ID"));
+		} catch (Exception e) {
+			System.err.println("Crew_board_writer_id = null");
+			crew_boardDTO.setCrew_board_writer_id(null);
+		}
+		try {
+			crew_boardDTO.setCrew_board_title(resultSet.getString("CREW_BOARD_TITLE"));
+		} catch (Exception e) {
+			System.err.println("Crew_board_title = null");
+			crew_boardDTO.setCrew_board_title(null);
+		}
 		return crew_boardDTO;
-	};
-}
-
-class CrewBoardRowMapperAll implements RowMapper<Crew_boardDTO> {
-
-	public Crew_boardDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
-		Crew_boardDTO crew_boardDTO=new Crew_boardDTO();
-		System.out.print("GymCountRowMapper DB에서 가져온 데이터 {");
-		crew_boardDTO.setCrew_board_num(rs.getInt("CREW_BOARD_NUM"));
-		System.err.println("crew_board_num = ["+crew_boardDTO.getCrew_board_num()+"]");
-		crew_boardDTO.setCrew_board_writer_id(rs.getString("CREW_BOARD_WRITER_ID"));
-		System.err.println("crew_board_writer_id = ["+crew_boardDTO.getCrew_board_writer_id()+"]");
-		crew_boardDTO.setCrew_board_title(rs.getString("CREW_BOARD_TITLE"));
-		System.err.println("crew_board_title = ["+crew_boardDTO.getCrew_board_title()+"]");
-		crew_boardDTO.setCrew_board_content(rs.getString("CREW_BOARD_CONTENT"));
-		System.err.println("crew_board_content = ["+crew_boardDTO.getCrew_board_content()+"]");
-		crew_boardDTO.setCrew_board_cnt(rs.getInt("CREW_BOARD_CNT"));
-		System.err.println("crew_board_cnt = ["+crew_boardDTO.getCrew_board_cnt()+"]");
-		crew_boardDTO.setCrew_board_member_profile(rs.getString("MEMBER_PROFILE"));
-		System.err.print("crew_board_member_profile = ["+crew_boardDTO.getCrew_board_member_profile()+"]");
-		System.out.println("}");
-		return crew_boardDTO;
-	};
+	}
 }
