4dcf677c2322896863f1dda22b2c113bd84915bc
김종민 <jongmen124@naver.com>
2024-10-26 21:42:23 +0900

    [Service 추가] 서비스 템플릿 추가

---
+++ src/main/java/com/coma/app/biz/admin/MainManagementService.java
@@ -0,0 +1,7 @@
+package com.coma.app.biz.admin;
+
+import org.springframework.stereotype.Service;
+
+@Service("MainManagementService")
+public class MainManagementService {
+}
---
+++ src/main/java/com/coma/app/biz/admin/UserManagementService.java
@@ -0,0 +1,8 @@
+package com.coma.app.biz.admin;
+
+import org.springframework.stereotype.Service;
+
+@Service("UserManagementService")
+public class UserManagementService {
+
+}
