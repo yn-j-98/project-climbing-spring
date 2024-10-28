94d4e3ed1ab8c84d57f5bde2c14be0db8bf0ca71
김종민 <jongmen124@naver.com>
2024-10-28 16:50:51 +0900

    [코드 수정] -종민 241028 VIEW 리스너 코드 변경

--- src/main/java/com/coma/app/biz/common/SampleListener.java
+++ src/main/java/com/coma/app/biz/common/SampleListener.java
@@ -1,75 +1,113 @@
 package com.coma.app.biz.common;
 
 import java.util.ArrayList;
-import jakarta.servlet.ServletContextEvent;
-import jakarta.servlet.ServletContextListener;
-import jakarta.servlet.annotation.WebListener;
 
-import com.coma.app.biz.board.BoardDAO;
+import com.coma.app.biz.board.BoardService;
 import com.coma.app.biz.board.BoardDTO;
-import com.coma.app.biz.gym.GymDAO;
+import com.coma.app.biz.gym.GymService;
 import com.coma.app.biz.gym.GymDTO;
 import com.coma.app.biz.product.ProductDTO;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import jakarta.servlet.ServletContext;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
-@WebListener
-public class SampleListener implements ServletContextListener {
+public class SampleListener {
 
-	@Autowired
-	private BoardDAO boardDAO; // TODO 서비스로 바꿔야함
-	@Autowired
-	private GymDAO gymDAO; // TODO 서비스로 바꿔야함
-	@Autowired
-	private Crawling2 crawling;
+    private final BoardService boardService;
+    private final GymService gymService;
+    private final Crawling2 crawling;
+    private final ServletContext servletContext; //어플리케이션에 product정보를 담기위해 사용
+    private final ApplicationContext applicationContext; //빈 파괴를 위해 사용
 
-	@Override
-	public void contextInitialized(ServletContextEvent sce)  {
-		// 웹 서버 구동(실행)시 한번 수행될 코드 부분
-		System.out.println("com.coma.app.biz.common.SampleListener 시작");
+    @Autowired
+    public SampleListener(BoardService boardService, GymService gymService, Crawling2 crawling, ServletContext servletContext, ApplicationContext applicationContext) {
+        //필드 주입보다는 생성자 주입이 더 권장되는 방식이며, 테스트와 유지보수 측면에서도 생성자 주입이 더 유리
+        //컨테이너가 주입하지않기때문에 테스트 하기에 유리, 의존 주입 강제로 잠재적 에러 방지
+        this.boardService = boardService;
+        this.gymService = gymService;
+        this.crawling = crawling;
+        this.servletContext = servletContext;
+        this.applicationContext = applicationContext;
+    }
 
-		ArrayList<ProductDTO> product_datas = crawling.makeSampleProduct();
-		// 샘플 데이터 DB에 저장하는 코드
+    @PostConstruct
+    public void init() {
+        //PostConstruct : 스프링 컨텍스트 초기화 된 이후에 실행
+        //Application Context : 스프링 Context 기능의 중심인 최상위 인터페이스
 
-		// 전체 게시판 크롤링 부분
-		BoardDTO boardDTO = new BoardDTO();
-		BoardDTO data = boardDAO.selectOneCount(boardDTO);
-		if(data.getBoard_total() <= 0) {
-			System.out.println("com.coma.app.biz.common.SampleListener.makeSampleBoard 크롤링 시작");
-			ArrayList<BoardDTO> crawling_board_datas = crawling.makeSampleBoard();
-			for (BoardDTO board_data : crawling_board_datas) {
-				boardDAO.insert(board_data);
-			}
-		}
+        //SerlvetContext : 자바 자체의 Context를 말함
+        //스프링도 자바로 만들어졌으므로, 모든 스프링 Context는 ServletContext
 
-		// 상품 크롤링 부분
-		int PK = 0; // 자동부여하는 PK
-		for (ProductDTO product_data : product_datas) {
-			product_data.setProduct_num(PK);
-			PK++;
-		}
+        //applicationContext.xml 초기화 : 애플리케이션 전반에 걸친 모든 서비스, 리포지토리, 비즈니스 로직 빈이 생성되고 의존성 주입
+        //의존성이 주입된 후 각 빈에 대해 @PostConstruct 메서드가 실행
 
-		// 암벽장 크롤링 부분
-		GymDTO gymDTO = new GymDTO();
-		GymDTO data2 = gymDAO.selectOneCount(gymDTO);
-		if (data2.getGym_total() <= 0) {
-			System.out.println("com.coma.app.biz.common.SampleListener.makeSampleGym 크롤링 시작");
-			ArrayList<GymDTO> crawling_gym_datas = crawling.makeSampleGym();
-			for (GymDTO gym_data : crawling_gym_datas) {
-				gymDAO.insert(gym_data);
-			}
-		}
+        //이후 DispatcherServlet 초기화 진행
+        log.info("com.coma.app.biz.common.SampleListener 시작");
 
-		// 세션단위로 보내려면 ServletContextEvent 사용해서 전달해야 한다
-		sce.getServletContext().setAttribute("product_datas", product_datas); // FIXME TODO
-		crawling.close_driver();
-		System.out.println("com.coma.app.biz.common.SampleListener 끝");
-	}
+        ArrayList<ProductDTO> product_datas = crawling.makeSampleProduct();
+        // 상품 크롤링 부분
+        int PK = 0; // 자동부여하는 PK
+        for (ProductDTO product_data : product_datas) {
+            product_data.setProduct_num(PK);
+            PK++;
+        }
 
-	@Override
-	public void contextDestroyed(ServletContextEvent sce)  {
-		// 웹 서버 종료시 한번 수행될 코드 부분
-		// ex) DB 연결 해제
-	}
+        // 전체 게시판 크롤링 부분
+        BoardDTO boardDTO = new BoardDTO();
+        BoardDTO data = boardService.selectOneCount(boardDTO);
+        if (data.getTotal() <= 0) { //게시글 데이터가 하나도 없다면 크롤링 동작
+            log.info("com.coma.app.biz.common.SampleListener.makeSampleBoard 크롤링 시작");
+            ArrayList<BoardDTO> crawling_board_datas = crawling.makeSampleBoard();
+            for (BoardDTO board_data : crawling_board_datas) {
+                boardService.insert(board_data);
+            }
+        }
+
+
+        // 암벽장 크롤링 부분
+        GymDTO gymDTO = new GymDTO();
+        GymDTO data2 = gymService.selectOneCount(gymDTO);
+        if (data2.getTotal() <= 0) { //암벽장 데이터가 하나도 없다면 크롤링 동작
+            log.info("com.coma.app.biz.common.SampleListener.makeSampleGym 크롤링 시작");
+            ArrayList<GymDTO> crawling_gym_datas = crawling.makeSampleGym();
+            for (GymDTO gym_data : crawling_gym_datas) {
+                gymService.insert(gym_data);
+            }
+        }
+
+        // 어플리케이션단위로 보내려면 ServletContextEvent 사용해서 전달해야 한다
+        servletContext.setAttribute("product_datas", product_datas);
+        if (crawling != null) {
+            crawling.close_driver();
+        }
+        log.info("com.coma.app.biz.common.SampleListener 끝");
+
+        //수동 빈 파괴 메서드
+        destroy();
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        log.info("sampleListener 생명주기가 끝났을때 나오는 로그 = sampleListener 완전 파괴");
+    }
+
+    public void destroy() {
+        log.info("SampleListener 제거");
+        if (applicationContext instanceof ConfigurableApplicationContext) {
+            //ConfigurableApplicationContext : ApplicationContext의 서브인터페이스
+            //동적으로 빈을 등록하거나 제거할 수 있는 메서드 제공
+            ((ConfigurableApplicationContext) applicationContext).getBeanFactory().destroyBean(this);
+            //타입 캐스팅을 통해 더 구체적인 타입으로 변환할 때 괄호 사용
+            //다운 캐스팅
+            log.info("SampleListener 빈 ApplicationContext에서 제거완료");
+        }
+    }
 }
