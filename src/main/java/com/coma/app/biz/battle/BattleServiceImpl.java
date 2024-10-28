53380e869eb3c264c150fc56b6d96d4a35f19740
developerSeonah <salj3895.developer@gmail.com>
2024-10-28 11:15:01 +0900

    [코드 수정] - 선아 20241028 boardDAO - 오타 수정 BoardService,BoardServiceImpl,BattleService,BattleServiceImpl - 비즈니스 메서드 추가

--- src/main/java/com/coma/app/biz/battle/BattleServiceImpl.java
+++ src/main/java/com/coma/app/biz/battle/BattleServiceImpl.java
@@ -21,6 +21,21 @@ public class BattleServiceImpl implements BattleService{
 	public List<BattleDTO> selectAllBattleAllTop4(BattleDTO battleDTO) {return this.battleDAO.selectAllBattleAllTop4(battleDTO);}
 
 	@Override
+	public List<BattleDTO> selectAllSearchBattleNum(BattleDTO battleDTO) {return this.battleDAO.selectAllSearchBattleNum(battleDTO);}
+
+	@Override
+	public List<BattleDTO> selectAllSearchBattleName(BattleDTO battleDTO) {return this.battleDAO.selectAllSearchBattleName(battleDTO);}
+
+	@Override
+	public List<BattleDTO> selectAllBattle(BattleDTO battleDTO) {return this.battleDAO.selectAllBattle(battleDTO);}
+
+	@Override
+	public List<BattleDTO> selectAllWinBattle(BattleDTO battleDTO) {return this.battleDAO.selectAllWinBattle(battleDTO);}
+
+	@Override
+	public List<BattleDTO> selectAllCrewMemberName(BattleDTO battleDTO) {return this.battleDAO.selectAllCrewMemberName(battleDTO);}
+
+	@Override
 	public BattleDTO selectOneSearchMemberBattle(BattleDTO battleDTO) {return this.battleDAO.selectOneSearchMemberBattle(battleDTO);}
 
 	@Override
@@ -28,6 +43,9 @@ public class BattleServiceImpl implements BattleService{
 
 	@Override
 	public BattleDTO selectOneCountActive(BattleDTO battleDTO) {return this.battleDAO.selectOneCountActive(battleDTO);}
+
+	@Override
+	public BattleDTO selectOneCountActiveBattle(BattleDTO battleDTO) {return this.battleDAO.selectOneCountActiveBattle(battleDTO);}
 
 	@Override
 	public boolean insert(BattleDTO battleDTO) {
