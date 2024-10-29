b67feab58535a361f9ca7bc5880a6b0602b2b1a9
sunooou <rlatjsgh373@gmail.com>
2024-10-29 09:56:37 +0900

    [코드변경] - 선호 241029 DB GymController.java - 메서드 수정 GymDAO - CRUD수정 ReservationManagementController.java - 메서드 수정

--- src/main/java/com/coma/app/view/admin/ReservationManagementController.java
+++ src/main/java/com/coma/app/view/admin/ReservationManagementController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 @Controller
-public class ReservationAdminController {
+public class ReservationManagementController {
 
     @Autowired
     private ReservationService reservationService;
