fe0c89dea6758c25faa4365c59322012edbd8c9a
yn-j-98 <yena092582@gmail.com>
2024-10-29 16:07:22 +0900

    [코드 수정] - yena 241029 Controller

--- src/main/java/com/coma/app/view/member/LoginAspect.java
+++ src/main/java/com/coma/app/view/member/LoginAspect.java
@@ -1,6 +1,7 @@
 package com.coma.app.view.member;
 
 import com.coma.app.view.annotation.LoginCheckImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -13,6 +14,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
+
+@Slf4j
 @Aspect
 @Component
 public class LoginAspect {
@@ -27,6 +30,7 @@ public class LoginAspect {
     // @Around 어드바이스는 @LoginCheck 어노테이션이 붙은 메서드를 가로채서 로그인 체크를 수행
     @Around("@annotation(com.coma.app.view.annotation.LoginCheck)")
     public Object checkLogin(ProceedingJoinPoint pjp) throws Throwable {
+        log.info("@around Advice Start");
         // 사용자의 요청 정보를 검사 & 처리를 위해 필요해서 가져옴
         //현재 세션을 가져온 후, 세션에서 사용자 아이디 또는 인증 토큰을 확인
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
@@ -40,6 +44,7 @@ public class LoginAspect {
         loginCheckImpl.checkLogin(request, response, session);
 
         // 로그인된 경우 원래 메서드를 실행
+        log.info("@around Advice End");
         return pjp.proceed();
     }
 }
