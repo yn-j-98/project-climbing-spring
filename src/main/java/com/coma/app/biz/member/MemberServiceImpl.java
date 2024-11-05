9c4515c8336a04425209c587b3fef99285aa4e33
junyeol <junyeoli75@gmail.com>
2024-11-05 21:40:00 +0900

    [로직수정] junyeol 2411105 controller memberDAO를 mybatis로 변경

--- src/main/java/com/coma/app/biz/member/MemberServiceImpl.java
+++ src/main/java/com/coma/app/biz/member/MemberServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 public class MemberServiceImpl implements MemberService {
 	
 	@Autowired
-	private MemberDAO memberDAO;
+	private MemberDAO3 memberDAO;
 
 
 	@Override
